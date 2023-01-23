c562d729c5b3e73d3f1934f7e35dfcea6560f1f6
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-22 17:44:19 -0800

    adding

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
